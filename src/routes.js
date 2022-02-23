c907cb140bab636fc48a951b56901637134bb339
Fabricio Oliveira <oliveirafbc@gmail.com>
2022-02-23 10:31:19 -0300

    final aula 1

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,30 @@
+// Bibliote para criar servidor
 const express = require('express');
+// vai criar as rotas/caminhos
 const routes = express.Router()
 
 // request , response
-routes.get('/', (request, response) => {
+/*routes.get('/', (request, response) => {
     return response.sendFile(__dirname + "/views/index.html")
-})
+})*/
 
-routes.get('/index.html', (req, res) => {
-    return res.redirect('/')
-})
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Fabricio",
+    avatar: "https://avatars.githubusercontent.com/u/34220709?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+
+// Não envia mais o html pronto
 
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+// permite usar contexto ejs no html  <% EJS AQUI   %>. Trocar todos html por ejs. Usando template engine
+server.set('view engine','ejs')
+
 // habilitar arquivos estaticos. Todos files dentro de public em uma rota
 server.use(express.static("public"))
 
 server.use(routes)
-// ATIVAR server:    npm run dev
-
-
 
 server.listen(3000,() => console.log('rodando..'))
 
+// ATIVAR server:    npm run dev
+// PAREI MINUTO 3:45:00
 
 
 
